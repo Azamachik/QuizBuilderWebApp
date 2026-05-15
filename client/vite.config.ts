58576e01d63df30be8c3f2f6deda78c37cda90df
Azamat <azamat-karimov-05@mail.ru>
2026-05-15 14:38:07 +0300

    config: update api

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   },
   define: {
     __IS_DEV__: JSON.stringify(true),
-    __API__: JSON.stringify('quizbuilderserver-mhjcsfc7.b4a.run/'),
+    __API__: JSON.stringify('https://quizbuilderserver-mhjcsfc7.b4a.run/'),
     __PROJECT__: JSON.stringify('frontend')
   },
   test: {
